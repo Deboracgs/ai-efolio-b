f898faa49d6c4d2fd5267dd90b49131344013b72
Debora <debora.goncalves@vigil365.com>
2020-05-11 19:13:55 +0100

    render cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import Poker from './poker';
 
 class App extends React.Component {
-  private poker: Poker = new Poker();
+  private poker: Poker = new Poker([13, 8, 2, 19, 45, 31, 13, 9, 14, 52, 37, 47, 24, 42, 8, 7]);
   componentDidMount(){
     this.poker.initGame();
   }
--- src/poker.ts
+++ src/poker.ts
@@ -1,31 +1,52 @@
+const rangeCards = {
+    a: [1,13],
+    b: [14,26],
+    c: [27,39],
+    d: [40,52]
+};  
+interface ScientistInProject {
+    id: string;
+    count: number;
+}
+
+interface Project {
+    scientst: string[];
+    production: number;
+}
 export default class Poker {
     private zScientistLimitMayBeInProject: number;
     private wScientistPerProject: number;
     private kScientistQuantity: number;
     private gGroupOfCards: number;
-    private naipeA: any;
-    private naipeB: any;
-    private naipeC: any;
-    private naipeD: any;
+    private scientist: string[];
+    private cards: number[];
+    private naipeA: number[];
+    private naipeB: number[];
+    private naipeC: number[];
+    private naipeD: number[];
+    private scientistInProject: ScientistInProject[];
+    private projects: Project[];
 
-    constructor(){
-        this.zScientistLimitMayBeInProject = 2;
-        this.wScientistPerProject = 8;
-        this.kScientistQuantity = 13;
-        this.gGroupOfCards = 5;
+    constructor(instances: number[]){
+        this.scientist = [];
+        this.kScientistQuantity = instances[0];   
+        this.wScientistPerProject = instances[1];
+        this.zScientistLimitMayBeInProject = instances[2];
+        this.gGroupOfCards = 5;   // DON'T CHANGE
+        this.cards = instances.slice(3,instances.length);
+        this.scientistInProject = [];
+        this.projects = []
 
-        // DON'T CHANGE
-
-        this.naipeA = this.generateArrayOfNumbers(1,13);
-        this.naipeB = this.generateArrayOfNumbers(14,26);
-        this.naipeC = this.generateArrayOfNumbers(27,39);
-        this.naipeD = this.generateArrayOfNumbers(40,52);
+        this.naipeA = this.generateArrayOfNumbers(rangeCards.a);
+        this.naipeB = this.generateArrayOfNumbers(rangeCards.b);
+        this.naipeC = this.generateArrayOfNumbers(rangeCards.c);
+        this.naipeD = this.generateArrayOfNumbers(rangeCards.d);
 
     }
 
-    generateArrayOfNumbers(initial: number, latest: number): Number[] {
+    generateArrayOfNumbers(range: number[]): number[] {
         let arr = [];
-        for (let index = initial; index <= latest; index++) {
+        for (let index = range[0]; index <= range[1]; index++) {
             arr.push(index);
         }
         return arr;
@@ -33,25 +54,84 @@ export default class Poker {
 
     generateProjects(){
         let maxOfProjects = Math.floor(this.wScientistPerProject * this.zScientistLimitMayBeInProject / this.gGroupOfCards);
-        console.log(maxOfProjects)
 
         for (let index = 1; index <= maxOfProjects; index++) {
-            console.log(`Projeto ${index}: `);
+            let scientistProject: string[] = [];
+
+            this.scientist.map((value, i) => {
+               let scientistIndex = this.scientistInProject.findIndex((x) => x.id === value);
+               if(scientistProject.length < this.gGroupOfCards){
+                    if(scientistIndex !== -1){
+                        if(this.scientistInProject[scientistIndex].count !== 2){
+                            scientistProject.push(value);
+                            this.scientistInProject[scientistIndex].count = this.scientistInProject[scientistIndex].count +1;
+                        }
+                    }else{
+                        scientistProject.push(value);
+                        this.scientistInProject.push({ count: 1, id: value });
+                    }
+               }
+            });
+            this.projects.push({scientst: scientistProject, production: 0});
         }
     }
 
+    getRange(value: number): string {
+        let isA, isB, isC, isD;
+        
+        isA = this.naipeA.findIndex(x => x === value);
+        isB = this.naipeB.findIndex(x => x === value);
+        isC = this.naipeC.findIndex(x => x === value);
+        isD = this.naipeD.findIndex(x => x === value);
+        
+        if(isA !== -1){
+            return `A${isA}`
+        }else if(isB !== -1){
+            return `B${isB}`
+        }else if(isC !== -1){
+            return `C${isC}`
+        }else if(isD !== -1){
+            return `D${isD}`
+        }
+        return '';
+    }
+
+    generateScientistCards(){
+        this.cards.map((value): void => {
+            this.scientist.push(this.getRange(value));
+        });
+        console.log(`Cientistas (#${this.zScientistLimitMayBeInProject}): ${this.scientist}`);
+    }
+
+    renderTeam(){
+        let text: string = "";
+        this.scientistInProject.map((value) => {
+            text = `${text} ${value.id}#${value.count}`
+        });
+        console.log(`Equipa #${this.wScientistPerProject}: ${text}`)
+    }
+
+    renderProjects(){
+        this.projects.map((value, i): void => {
+            console.log(`Projeto ${i+1}: ${value.scientst} [Producao]: ${value.production}`)
+        })
+    }
 
     generateInstance(){
-        console.log(`Cientistas (#${this.zScientistLimitMayBeInProject}): `);
-        console.log(`Equipa #${this.wScientistPerProject}: `);
-        console.log(`Equipa #${this.naipeA}: `);
-        console.log(`Equipa #${this.naipeB}: `);
-        console.log(`Equipa #${this.naipeC}: `);
-        console.log(`Equipa #${this.naipeD}: `);
-        this.generateProjects();
+        
+        // console.log(`Equipa #${this.wScientistPerProject}: `);
+        // console.log(`Equipa #${this.naipeA}: `);
+        // console.log(`Equipa #${this.naipeB}: `);
+        // console.log(`Equipa #${this.naipeC}: `);
+        // console.log(`Equipa #${this.naipeD}: `);
+        
     }
 
     public initGame(): any {
+        this.generateScientistCards();
+        this.generateProjects();
+        this.renderTeam();
+        this.renderProjects();
         this.generateInstance();
     }
 }
